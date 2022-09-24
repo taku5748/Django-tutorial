b40ff6f0cb58ea66c69f5f9dbc6668694cdd94dd
takuya_osawa <taku5748@outlook.jp>
2022-09-24 12:56:57 +0900

    committed

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
 
 def frontpage(request):
-    return render(request, "blog/frontpage.html")
+    posts = Post.objects.all()
+    return render(request, "blog/frontpage.html",{"posts": posts})
+
+def post_detail(request,slug):
+    post = Post.objects.get(slug=slug)
+    return render(request,"blog/post_detail.html",{"post":post})
     
