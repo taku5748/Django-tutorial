8c4ffed4249d09f82fc6da5b90da54620a0df6d1
takuya_osawa <taku5748@outlook.jp>
2022-09-24 21:51:43 +0900

    commit

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+
+from .forms import CommentForm
 from .models import Post
 
 # Create your views here.
@@ -9,5 +11,18 @@ def frontpage(request):
 
 def post_detail(request,slug):
     post = Post.objects.get(slug=slug)
-    return render(request,"blog/post_detail.html",{"post":post})
+
+    if request.method == "POST":
+        form = CommentForm(request.POST)
+
+    if form.is_valid():
+        comment = form.save(commit=False)
+        comment.post = post
+        comment.save()
+
+        return redirect("post_detail",slug = post.slug)
+    else:
+        form = CommentForm()
+
+    return render(request,"blog/post_detail.html",{"post":post, "form":form})
     
